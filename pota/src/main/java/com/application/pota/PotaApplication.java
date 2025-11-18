4b177fab41fa843e3a3b318289e73b06311189d5
Owen-Lianggara <6182301025@student.unpar.ac.id>
2025-11-18 20:15:38 +0700

    inital commit

---
+++ pota/src/main/java/com/application/pota/PotaApplication.java
@@ -0,0 +1,13 @@
+package com.application.pota;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PotaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PotaApplication.class, args);
+	}
+
+}
---
+++ pota/src/test/java/com/application/pota/PotaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.pota;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PotaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

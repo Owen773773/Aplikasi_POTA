8f1402fbbfc460a1a696e9aff225f45f386da897
Kenneth Nathanael <kenneth@ftis.unpar>
2025-11-25 09:46:23 +0700

    jadwalFix

--- pota/src/main/java/com/application/pota/jadwal/JadwalRepository.java
+++ pota/src/main/java/com/application/pota/jadwal/JadwalRepository.java
@@ -6,27 +6,30 @@ import java.util.List;
 
 public interface JadwalRepository {
 
-    //Mencari Jadwal berdasarkan idPengguna dan tanggal
+    // Mencari Jadwal berdasarkan idPengguna dan tanggal
     List<Jadwal> findByIdPengguna(String IdPengguna, Date date);
 
-    //mencari Jadwal Berdasarkan idRuangan
+    // Mencari Jadwal Berdasarkan idRuangan
     List<Jadwal> findByDateByidRuangan(int idRuangan, Date date);
 
-    //mencari Jadwal Berdasarkan idBimbingan
+    // Mencari Jadwal Berdasarkan idBimbingan
     List<Jadwal> findByIdbimbingan(String idBim, Date date);
 
-    //Add Jadwal
+    // Add Jadwal
     void addJadwal(Jadwal targetDate);
 
-    //remove jadwal
+    // Remove jadwal
     void removeJadwal(int idJadwal);
 
-    //cari jadwal dalam 1 minggu by Pengguna
+    // Cari jadwal dalam 1 minggu by Pengguna (hanya jadwal pribadi)
     List<Jadwal> findByWeekRangePengguna(LocalDate startOfWeek, LocalDate endOfWeek, String IdPengguna);
 
-    //cari jadwal dalam 1 minggu by Ruangan (pemblokiran)
+    // Cari jadwal dalam 1 minggu by Ruangan (pemblokiran)
     List<Jadwal> findByWeekRangeRuangan(LocalDate startOfWeek, LocalDate endOfWeek, int idRuangan);
 
-    //cari jadwal bimbingan dalam 1 minggu by Ruangan dengan status
+    // Cari jadwal bimbingan dalam 1 minggu by Ruangan dengan status
     List<JadwalJdbc.JadwalWithStatus> findBimbinganByWeekRangeRuangan(LocalDate startOfWeek, LocalDate endOfWeek, int idRuangan);
+
+    // Cari jadwal bimbingan dalam 1 minggu by Pengguna dengan status
+    List<JadwalJdbc.JadwalWithStatus> findBimbinganByWeekRangePengguna(LocalDate startOfWeek, LocalDate endOfWeek, String idPengguna);
 }

1310ad73d9d452a6a006ae83db62e4c9b473266b
Owen-Lianggara <6182301025@student.unpar.ac.id>
2025-12-09 20:49:57 +0700

    final

---
+++ pota/src/main/java/com/application/pota/bimbingan/BimbinganDosenDashboard.java
@@ -0,0 +1,22 @@
+package com.application.pota.bimbingan;
+
+import com.application.pota.mahasiswa.Mahasiswa;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Time;
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class BimbinganDosenDashboard {
+    private LocalDate DateNow;
+    private Time waktuMulai;
+    private Time waktuSelesai;
+    private String lokasi;
+    private String dosen    ;
+    private String mahasiswa;
+}

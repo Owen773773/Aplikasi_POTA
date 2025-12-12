66e73f92485edf916829ef2cf3259cbed2e22e4f
Owen-Lianggara <6182301025@student.unpar.ac.id>
2025-12-12 22:33:11 +0700

    final

--- pota/src/main/java/com/application/pota/export/ExportJdbc.java
+++ pota/src/main/java/com/application/pota/export/ExportJdbc.java
@@ -16,59 +16,103 @@ public class ExportJdbc implements ExportRepository {
     @Override
     public List<KelayakanSidangDTO> getDataKelayakan() {
         String sql = """
-            SELECT
-                p.nama AS nama_mahasiswa,
-                ta.TopikTA AS topik,
-                (
-                    SELECT COUNT(*)
-                    FROM TopikBimbingan tb
-                    JOIN PenjadwalanBimbingan pb ON tb.IdBim = pb.IdBim
-                    JOIN Jadwal j ON pb.IdJadwal = j.IdJadwal
-                    WHERE tb.IdTA = ta.IdTA
-                      AND j.tanggal < ta.TanggalUTS
-                ) AS pra_uts,
-                (
-                    SELECT COUNT(*)
-                    FROM TopikBimbingan tb
-                    JOIN PenjadwalanBimbingan pb ON tb.IdBim = pb.IdBim
-                    JOIN Jadwal j ON pb.IdJadwal = j.IdJadwal
-                    WHERE tb.IdTA = ta.IdTA
-                      AND j.tanggal >= ta.TanggalUTS
-                ) AS pasca_uts,
+            WITH CurrentDate AS (
+                SELECT
+                DATE '2025-12-12' AS today_date
+                ),
+                MahasiswaTA AS (
+                SELECT
+                    P.nama AS nama_mahasiswa,
+                    TA.IdTA,
+                    TA.TopikTA,
+                    TA.TanggalUTS,
+                    TA.TanggalUas,
+                    A.idSemester,
+                    A.minimumPra,
+                    A.minimumPasca
+                FROM
+                    Pengguna P
+                JOIN
+                    Mahasiswa M ON P.IdPengguna = M.IdPengguna
+                JOIN
+                    TugasAkhir TA ON M.IdPengguna = TA.IdMahasiswa
+                JOIN
+                    TAtermasukAkademik TAA ON TA.IdTa = TAA.IdTA
+                JOIN
+                    Akademik A ON TAA.idAkademik = A.idSemester
+                ),
+                BimbinganCount AS (
+                SELECT
+                    MTA.IdTA,
+                    COUNT(CASE
+                        WHEN J.tanggal < MTA.TanggalUTS THEN 1
+                        ELSE NULL
+                    END) AS jumlah_bim_pra_uts,
+                    COUNT(CASE
+                        WHEN J.tanggal >= MTA.TanggalUTS AND J.tanggal <= MTA.TanggalUas THEN 1
+                        ELSE NULL
+                    END) AS jumlah_bim_pasca_uts
+                FROM
+                    MahasiswaTA MTA
+                JOIN
+                    TopikBimbingan TB ON MTA.IdTA = TB.IdTA
+                JOIN
+                    PenjadwalanBimbingan PB ON TB.IdBim = PB.IdBim
+                JOIN
+                    Jadwal J ON PB.IdJadwal = J.IdJadwal
+                WHERE
+                    TB.StatusBimbingan = 'Selesai' -- Hanya hitung bimbingan yang sudah 'Selesai'
+                GROUP BY
+                    MTA.IdTA
+                )
+                SELECT
+                MTA.nama_mahasiswa,
+                MTA.TopikTA AS topik_tugas_akhir,
+                COALESCE(BC.jumlah_bim_pra_uts, 0) AS jumlah_bimbingan_pra_uts,
+                COALESCE(BC.jumlah_bim_pasca_uts, 0) AS jumlah_bimbingan_pasca_uts,
                 CASE
-                    WHEN
-                        (
-                            SELECT COUNT(*)
-                            FROM TopikBimbingan tb
-                            JOIN PenjadwalanBimbingan pb ON tb.IdBim = pb.IdBim
-                            JOIN Jadwal j ON pb.IdJadwal = j.IdJadwal
-                            WHERE tb.IdTA = ta.IdTA
-                              AND j.tanggal < ta.TanggalUTS
-                        ) >= ak.minimumPra
-                    AND
-                        (
-                            SELECT COUNT(*)
-                            FROM TopikBimbingan tb
-                            JOIN PenjadwalanBimbingan pb ON tb.IdBim = pb.IdBim
-                            JOIN Jadwal j ON pb.IdJadwal = j.IdJadwal
-                            WHERE tb.IdTA = ta.IdTA
-                              AND j.tanggal >= ta.TanggalUTS
-                        ) >= ak.minimumPasca
-                    THEN 'Layak' ELSE 'Tidak Layak'
-                END AS kelayakan
-            FROM TugasAkhir ta
-            JOIN Pengguna p ON ta.IdMahasiswa = p.IdPengguna
-            JOIN TAtermasukAkademik tak ON ta.IdTA = tak.IdTA
-            JOIN Akademik ak ON tak.idAkademik = ak.idSemester;
+                    WHEN CD.today_date < MTA.TanggalUTS THEN
+                        'Pra-UTS'
+                    WHEN CD.today_date >= MTA.TanggalUTS AND CD.today_date <= MTA.TanggalUas THEN
+                        'Pasca-UTS'
+                    ELSE
+                        'Selesai Periode'
+                END AS periode_saat_ini,
+                CASE
+                    WHEN CD.today_date < MTA.TanggalUTS THEN
+                        CASE
+                            WHEN COALESCE(BC.jumlah_bim_pra_uts, 0) >= MTA.minimumPra THEN 'Layak'
+                            ELSE 'Tidak Layak'
+                        END
+                    WHEN CD.today_date >= MTA.TanggalUTS AND CD.today_date <= MTA.TanggalUas THEN
+                        CASE
+                            WHEN COALESCE(BC.jumlah_bim_pasca_uts, 0) >= MTA.minimumPasca THEN 'Layak'
+                            ELSE 'Tidak Layak'
+                        END
+                    ELSE
+                        'N/A (Periode berakhir)'
+                END AS status_kelayakan,
+                CASE
+                    WHEN CD.today_date < MTA.TanggalUTS THEN MTA.minimumPra
+                    WHEN CD.today_date >= MTA.TanggalUTS AND CD.today_date <= MTA.TanggalUas THEN MTA.minimumPasca
+                    ELSE NULL
+                END AS minimum_bimbingan_yang_diperlukan
+                FROM
+                    MahasiswaTA MTA
+                CROSS JOIN
+                    CurrentDate CD
+                LEFT JOIN
+                    BimbinganCount BC ON MTA.IdTA = BC.IdTA;
         """;
 
         return jdbcTemplate.query(sql, (rs, rowNum) -> {
             KelayakanSidangDTO dto = new KelayakanSidangDTO();
             dto.setNama(rs.getString("nama_mahasiswa"));
-            dto.setTopik(rs.getString("topik"));
-            dto.setPraUts(rs.getInt("pra_uts"));
-            dto.setPascaUts(rs.getInt("pasca_uts"));
-            dto.setKelayakan(rs.getString("kelayakan"));
+            dto.setTopik(rs.getString("topik_tugas_akhir")); // <-- KOREKSI
+            dto.setPraUts(rs.getInt("jumlah_bimbingan_pra_uts")); // <-- KOREKSI
+            dto.setPascaUts(rs.getInt("jumlah_bimbingan_pasca_uts")); // <-- KOREKSI
+            dto.setKelayakan(rs.getString("status_kelayakan")); // <-- KOREKSI
+
             return dto;
         });
     }
